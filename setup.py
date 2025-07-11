912a05abae9f119baf24cf7358e49571ade4ee96
dfoley84 <davidfoley84@gmail.com>
2025-07-11 20:51:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="alb-listener-rule",
     version="0.1.0",
-    packages=["ListenerRole", "ListenerRole.alb_listener_rule"],
+    packages=find_packages(),
     install_requires=[
         "aws-cdk-lib>=2.0.0",
         "constructs>=10.0.0",
     ],
     author="Your Name",
     description="Reusable CDK construct to add ALB listener rules",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
